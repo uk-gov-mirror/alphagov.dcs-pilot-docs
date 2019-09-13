869c5283bc748c344b72b966047f5a962aa0bc40
Andrea Szöllössi <andrea.szollossi@digital.cabinet-office.gov.uk>
2019-09-13 21:45:49 +0100

    Remove `.html` from URLs
    Pretty URLs Middleman reference: 
    https://middlemanapp.com/advanced/pretty-urls/
    
    Changes http://example.com/about-us.html to http://example.com/about-us

--- config.rb
+++ config.rb
@@ -1,3 +1,7 @@
 require 'govuk_tech_docs'
 
+# Pretty URLs: Change http://example.com/about-us.html to http://example.com/about-us
+# Reference: https://middlemanapp.com/advanced/pretty-urls/
+activate :directory_indexes
+
 GovukTechDocs.configure(self)
